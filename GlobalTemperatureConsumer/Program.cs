56d5f7c6c32c7c90b8ca14b8d1f993851d7042e8
Bartlomiej Balazy <bartek0419@gmail.com>
2021-01-26 15:59:17 +0100

    Add blank console app

---
+++ GlobalTemperatureConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GlobalTemperatureConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

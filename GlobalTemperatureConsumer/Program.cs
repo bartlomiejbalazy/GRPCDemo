a4b5cfbcacdbd5a168978e731cdc4fcf83853e9f
Bartlomiej Balazy <bartek0419@gmail.com>
2021-01-26 16:24:00 +0100

    Add simple client

--- GlobalTemperatureConsumer/Program.cs
+++ GlobalTemperatureConsumer/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GRPCServer;
 
 namespace GlobalTemperatureConsumer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var globalTemperatureClient = new GlobalTemperature.GlobalTemperatureClient(channel);
+
+            using (var serverStreamingCall = globalTemperatureClient.GetMeasurementsForRegion(new GetMeasurementsRequest {RegionId = 1}))
+            {
+                while (await serverStreamingCall.ResponseStream.MoveNext(CancellationToken.None))
+                {
+                    var currentMeasurementModel = serverStreamingCall.ResponseStream.Current;
+                    Console.WriteLine($"RegionId : { currentMeasurementModel.RegionId } Current temperature: { currentMeasurementModel.Temperature }C");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
